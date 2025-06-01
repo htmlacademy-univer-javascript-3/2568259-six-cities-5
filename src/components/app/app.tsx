91a5620352e5c8117fe032c63af9b2ff86a781b9
Zyryanova Natalya <n_zyryanova@inbox.ru>
2025-06-02 02:05:28 +0500

    4.19

---
+++ src/components/app/app.tsx
@@ -0,0 +1,49 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Main from '../../pages/main/main';
+import { ListProps } from '../main/list-places/list-places';
+import { AppRoute, AuthorizationStatus } from '../../const';
+import Login from '../../pages/login/login';
+import Favorites from '../../pages/favorites/favorites';
+import Offer from '../../pages/offer/offer';
+import NotFound from '../../pages/not-found/not-found';
+import PrivateRoute from '../common/private-route/private-route';
+
+
+function App({places}: ListProps): JSX.Element {
+  const favoriteplaces = places.filter((place) => place.isFavorite);
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path = {AppRoute.Main}
+          element = {<Main places = {places}/>}
+        />
+        <Route
+          path= {AppRoute.Login}
+          element = {<Login/>}
+        />
+        <Route
+          path= {AppRoute.Favorite}
+          element = {
+            <PrivateRoute
+              authorizationStatus={AuthorizationStatus.NoAuth}
+            >
+              <Favorites places = {favoriteplaces}/>
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path= {AppRoute.Offer}
+          element = {<Offer/>}
+        />
+        <Route
+          path="*"
+          element={<NotFound />}
+        />
+      </Routes>
+    </BrowserRouter>
+
+  );
+}
+
+export default App;
---
+++ src/components/main/place-card/place-card.tsx
@@ -0,0 +1,65 @@
+import { OfferProps } from '../../../types/list-offers';
+
+
+type CardProps = {
+  place: OfferProps;
+  onCardHover: (id: string | null) => void;
+};
+
+function PlaceCard({place,onCardHover}:CardProps):JSX.Element {
+  const handleOnMouseEnter = () => onCardHover(place.id);
+  const handleOnMouseLeave = () => onCardHover(null);
+
+  return(
+    <article
+      onMouseEnter = {handleOnMouseEnter}
+      onMouseLeave = {handleOnMouseLeave}
+
+      className="cities__card place-card"
+    >
+      {
+        place.isPremium &&
+        <div className="place-card__mark">
+          <span>Premium</span>
+        </div>
+      }
+
+      <div className="cities__image-wrapper place-card__image-wrapper">
+        <a href="#">
+          <img className="place-card__image" src={place.previewImage} width="260" height="200" alt="Place image"/>
+        </a>
+      </div>
+      <div className="place-card__info">
+        <div className="place-card__price-wrapper">
+          <div className="place-card__price">
+            <b className="place-card__price-value">&euro;{place.price}</b>
+            <span className="place-card__price-text">&#47;&nbsp;night</span>
+          </div>
+          <button className= {
+            place.isFavorite ? 'place-card__bookmark-button place-card__bookmark-button--active button' :
+              'place-card__bookmark-button button'
+          }
+          type="button"
+          >
+            <svg className="place-card__bookmark-icon" width="18" height="19">
+              <use xlinkHref="#icon-bookmark"></use>
+            </svg>
+            <span className="visually-hidden">{place.isFavorite ? 'In bookmarks' : 'To bookmarks'}</span>
+          </button>
+        </div>
+        <div className="place-card__rating rating">
+          <div className="place-card__stars rating__stars">
+            <span style={{width: `${String(place.rating * 20)}%`}}></span>
+            <span className="visually-hidden">Rating</span>
+          </div>
+        </div>
+        <h2 className="place-card__name">
+          <a href="#">{place.title}</a>
+        </h2>
+        <p className="place-card__type">{place.type}</p>
+      </div>
+    </article>
+  );
+}
+
+export default PlaceCard;
